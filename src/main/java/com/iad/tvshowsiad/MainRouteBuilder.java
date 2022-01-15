1195c6fedcbda553c1c8fb6104d4821955e8aa39
IuliaBlindu <iuliaioana.blindu@gmail.com>
2022-01-15 19:29:16 +0200

    first part works - getting games data to files based on category

--- src/main/java/com/iad/tvshowsiad/MainRouteBuilder.java
+++ src/main/java/com/iad/tvshowsiad/MainRouteBuilder.java
@@ -1,9 +1,16 @@
 package com.iad.tvshowsiad;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.UUID;
 
+@Service
 public class MainRouteBuilder extends RouteBuilder {
 
     @Autowired
@@ -11,7 +18,47 @@ public class MainRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("jetty:https://0.0.0.0:%d");
+        fromF("jetty:http://0.0.0.0:%d/games/?matchOnUriPrefix=true", configuration.getPort())
+        .setHeader("category").header(Exchange.HTTP_PATH)
+        .choice()
+        .when(exchange ->
+            Paths.get(String.format("cache/%s.json",
+                                exchange.getIn().getHeader("category", String.class)))
+                        .toFile().exists()
+        )
+        .setBody(exchange -> {
+            return Paths.get(String.format("cache/%s.json", exchange.getIn().getHeader("category", String.class))).toFile();
+        })
+        .convertBodyTo(String.class)
+        .otherwise()
+            .process(x -> {
+                System.out.println(x);
+            })
+            .removeHeaders("*", "category")
+            .setHeader(Exchange.HTTP_URI).header("url")
+            .setHeader(Exchange.HTTP_METHOD).constant("GET")
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+            .setHeader("Accept", constant("application/json"))
+            .setHeader(Exchange.HTTP_QUERY)
+            .simple("category=${header.category}")
+            .to("https://www.freetogame.com/api/games")
+            .convertBodyTo(String.class)
+            .unmarshal().json(JsonLibrary.Jackson)
+            .setHeader("id", () -> UUID.randomUUID().toString())
+            .setHeader("size").simple("${body.size}")
+//            .setBody(exchange -> {
+//                List<Object> items = exchange.getIn().getBody(List.class);
+//                int count = items.size();
+//                return count;
+//            })
+            .marshal().json(JsonLibrary.Jackson)
+            .process(exchange -> {
+                Files.write(Paths.get(String.format("cache/%s.json", exchange.getIn().getHeader("category", String.class))),
+                        exchange.getIn().getBody(String.class).getBytes());
+            })
+            .setBody().constant("")
+            .setBody(exchange -> Paths.get(String.format("cache/%s.json", exchange.getIn().getHeader("category", String.class))).toFile())
+            .convertBodyTo(String.class);
     }
 
 
